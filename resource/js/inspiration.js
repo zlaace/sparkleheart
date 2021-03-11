749b860c08e85d77d2b5f31c590d745e16ff4a6d
Zane Lace <zane.laace@gmail.com>
2021-03-11 14:15:35 +0200

    Add sample-video and update .html files

--- resource/js/inspiration.js
+++ resource/js/inspiration.js
@@ -4,103 +4,103 @@ const stories = [
     id: 1,
     name: "Sibilla",
     img: "./assets/img/17.jpg",
-    video: "./assets/video/Sibilla.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 2,
     name: "Lauma",
     img: "./assets/img/11.jpg",
-    video: "./assets/video/Lauma.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 3,
     name: "Valentīna",
     img: "./assets/img/16.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 4,
     name: "Arta",
     img: "./assets/img/5.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 5,
     name: "Ketija",
     img: "./assets/img/10.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 6,
     name: "Anda",
     img: "./assets/img/6.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 7,
     name: "Asnāte",
     img: "./assets/img/4.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 8,
     name: "Indra",
     img: "./assets/img/8.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 9,
     name: "Alīna",
     img: "./assets/img/1.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 10,
     name: "Līga",
     img: "./assets/img/12.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 11,
     name: "Regīna",
     img: "./assets/img/14.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 12,
     name: "Marta",
     img: "./assets/img/13.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 13,
     name: "Zanda",
     img: "./assets/img/15.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 14,
     name: "Ilze",
     img: "./assets/img/7.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 15,
     name: "Katrīna",
     img: "./assets/img/9.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 16,
     name: "Gunita",
     img: "./assets/img/2.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 17,
     name: "Egija",
     img: "./assets/img/3.jpg",
-    video: "./assets/video/Sparkleheart.mp4"
+    video: "./assets/video/sample_video.mp4"
   },
   {
     id: 18,
